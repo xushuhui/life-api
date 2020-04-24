cedda9548f9ac400475a088cf5726934e8fed9c4
xushuhui <474497097@qq.com>
2020-04-24 17:45:03 +0800

    增加扩展包

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,48 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    protected $data = [];
+    protected $code = 0;
+    protected $message = 'OK';
+
+    public function succeed()
+    {
+        return $this->setCode($this->code);
+    }
+
+    public function fail($code)
+    {
+        return $this->setCode($code);
+
+    }
+
+    public function setCode($code)
+    {
+        $this->code = $code;
+        //TODO message
+        //$this->message = CodeTable::$table[$code];
+        return $this->toJson();
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+        $this->toJson();
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function setResult($code, $message)
+    {
+        $this->code    = $code;
+        $this->message = $message;
+    }
+
+    public function toJson()
+    {
+        return response()->json(['code' => $this->code, 'message' => $this->message, 'data' => $this->data]);
+    }
 }
