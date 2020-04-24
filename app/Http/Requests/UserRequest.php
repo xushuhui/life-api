3a112a502eceb9295647fd8d40c38849426dbbe1
xushuhui <474497097@qq.com>
2020-04-24 18:05:14 +0800

    update

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -24,7 +24,17 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
-        ];
+            'name' => 'required|between:3,25',
+            'password' => 'required|string|min:6',
+            'verification_key' => 'required|string',
+            'verification_code' => 'required|string',
+            ];
+    }
+    public function attributes()
+    {
+        return [
+            'verification_key' => '短信验证码 key',
+            'verification_code' => '短信验证码',
+            ];
     }
 }
