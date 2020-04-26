72d41855fd1a43a1edcbacaa1f72b9f0ff7457ba
xushuhui <474497097@qq.com>
2020-04-26 18:07:37 +0800

    增加数据库

---
+++ database/migrations/2020_04_26_015055_create_stores_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string("name",15)->comment("店铺名称");
+            $table->string("logo")->comment("店铺logo");
+            $table->string("photo")->comment("店铺照片");
+            $table->string("intro")->comment("介绍");
+            $table->tinyInteger("type")->comment("类型");
+            $table->tinyInteger("status")->comment("状态");
+            $table->timestamp("expire_at")->comment("过期时间");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
--- database/migrations/2020_04_26_015159_create_store_users_table.php
+++ database/migrations/2020_04_26_015159_create_store_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStoreUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('store_users', function (Blueprint $table) {
             $table->id();
-            $table->string('nickname');
-            $table->string('phone')->unique();
-            $table->string('weapp_openid')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -29,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('store_users');
     }
 }
