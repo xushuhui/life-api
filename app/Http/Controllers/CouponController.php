61afb0ff6709b4453cf8e8bb01e79b8016c4c0e5
xushuhui <474497097@qq.com>
2020-05-04 12:35:30 +0800

    完成接口文档

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -6,22 +6,47 @@ use Illuminate\Http\Request;
 
 class CouponController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/coupon/status/{status}", summary="用户优惠券列表",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         return $this->setData();
     }
-
-    public function store()
+    /**
+     * @OA\Get(
+     *     path="/api/coupon/store/{store_id}", summary="商家详情",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(int $store_id)
     {
         return $this->setData();
     }
-
+    /**
+     * @OA\Get(
+     *     path="/api/coupon/stores", summary="商家列表",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function stores()
     {
         return $this->setData();
     }
-
-    public function used()
+    /**
+     * @OA\Get(
+     *     path="/api/coupon/used/{coupon_id}", summary="已使用优惠券性情",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function used(int $coupon_id)
     {
         return $this->setData();
     }
--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -9,25 +9,51 @@ use App\Models\User;
 
 class LikeController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/like/stores", summary="收藏商家列表",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function stores()
     {
         $likes = request()->user()->likes()->with('likeable')->paginate(20);
         return $this->setData($likes);
     }
+    /**
+     * @OA\Post(
+     *     path="/api/like/store/{id}", summary="收藏商家",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(int $id)
     {
         $store = Store::query()->find($id);
         request()->user()->like($store);
         return $this->succeed();
     }
-
+    /**
+     * @OA\Post(
+     *     path="/api/like/coupon/{id}", summary="收藏优惠券",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function coupon(int $id)
     {
         $coupon = Coupons::query()->find($id);
         request()->user()->like($coupon);
         return $this->succeed();
     }
-
+    /**
+     * @OA\Get(
+     *     path="/api/like/coupons", summary="收藏优惠券列表",
+     *     @OA\Response(response="200", description="success")
+     * )
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function coupons()
     {
         $likes = request()->user()->likes()->with('likeable')->paginate(20);
