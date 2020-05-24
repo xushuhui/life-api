0af371f4e8930ed963bdd8f1c47f42201d9c9837
cnpscy <2278757482@qq.com>
2020-05-24 17:47:37 +0800

    1.会员表，分享人属于商家的员工还是会员分享。

--- database/migrations/2020_05_23_233833_add_field_into_users.php
+++ database/migrations/2020_05_23_233833_add_field_into_users.php
@@ -15,6 +15,7 @@ class AddFieldIntoUsers extends Migration
     {
         Schema::table('users',function (Blueprint $table) {
             $table->string('source', 100)->default('')->comment('来源，文字备注即可');
+            $table->tinyInteger('share_role')->default(0)->comment("分享人类型：0.会员；1.商家/员工");
         });
     }
 
@@ -27,6 +28,7 @@ class AddFieldIntoUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('source');
+            $table->dropColumn('share_role');
         });
     }
 }
